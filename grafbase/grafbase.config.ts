9a01fd56ba551dbbadf5379ddd88c2c11efd7266
Mark Small <mark.small@astrosat.net>
2023-06-29 23:20:32 +0100

    feat(api): Define grafbase models
    I've created a couple of models that will form the start of our
    grafbase integration. You can learn more about grafbase at
    
        https://grafbase.com

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,59 @@
+import { auth, config, g } from '@grafbase/sdk';
+
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+// const authProvider = auth.OpenIDConnect({
+//   issuer: process.env.ISSUER_URL ?? ''
+// })
+//
+// Define Data Models
+// https://grafbase.com/docs/database
+
+// @ts-ignore
+const User = g
+  .model('User', {
+    name: g.string(),
+    email: g.string().unique(),
+    avatarUrl: g.url(),
+    description: g.string().length({ min: 2, max: 1000 }).optional(),
+    githubUrl: g.url().optional(),
+    linkedInUrl: g.url().optional(),
+    projects: g
+      .relation(() => Project)
+      .list()
+      .optional(),
+  })
+  .auth((rules) => {
+    rules.public().read();
+  });
+
+// @ts-ignore
+const Project = g
+  .model('Project', {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read();
+    rules.private().create().delete().update();
+  });
+
+export default config({
+  schema: g,
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+});
